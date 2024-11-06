3f408861c068d3c26374de66cd78a0916f2a065d
Ubuntu User <toru753@gmail.com>
2024-11-06 12:39:37 +0900

    modified files

--- cuda/reduction_sum.cu
+++ cuda/reduction_sum.cu
@@ -6,6 +6,8 @@ n   : array size (<=1024)
 s   : array (shared memory)
 sum : sum (output)
 */
+//#include "orcwa.h"
+
 __device__
 void reduction_sum(int tid, int n, real_t *s, real_t *sum)
 {
@@ -22,3 +24,4 @@ void reduction_sum(int tid, int n, real_t *s, real_t *sum)
 		*sum = s[0];
 	}
 }
+
